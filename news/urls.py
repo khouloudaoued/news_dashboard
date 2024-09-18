4e5a12e2fe87da5bb64b42daf8f76053683b42b7
khouloud <khouloudaoued504@gmail.com>
2024-09-18 18:48:58 +0100

    First commit

--- news/urls.py
+++ news/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fetch-news/', views.fetch_news, name='fetch-news'),
+    path('fetch-news/', views.fetch_news, name='fetch-news'),  # ربط الفيو الخاص بجلب الأخبار
+    path('', views.fetch_news, name='home'),  # جعل الصفحة الرئيسية تظهر الأخبار
 ]
--- news/views.py
+++ news/views.py
@@ -1,23 +1,32 @@
+from django.shortcuts import render
 import requests
 from django.http import JsonResponse
-from .models import Article
+from newsapi import NewsApiClient
 
 def fetch_news(request):
-    api_key = 'ceb2a687505b41f79df271d0b821bed0'  
-    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={api_key}'
     
-    # Fetch data from the external news API
-    response = requests.get(url)
-    api_data = response.json()
+    newsApi = NewsApiClient(api_key='ceb2a687505b41f79df271d0b821bed0')
     
-    # Retrieve articles from the database
-    articles = Article.objects.all()
+   
+    headLines = newsApi.get_top_headlines(country='us')  
     
-    # Convert database articles into a list of dictionaries
-    articles_list = list(articles.values('title', 'description', 'url', 'published_at'))
+    articles = headLines['articles']
     
-    # Combine API data and database articles in the response
-    return JsonResponse({
-        'api_articles': api_data.get('articles', []),  # Extract 'articles' from API response
-        'db_articles': articles_list
-    })
+   
+    
+    desc = []
+    news = []
+    img = []
+
+   
+    for article in articles:
+       
+        desc.append(article.get('description', 'No description available'))
+        news.append(article.get('title', 'No title'))
+        img.append(article.get('urlToImage', 'No image available'))
+
+    
+    articles_list = zip( news, desc, img)
+    
+    
+    return render(request, "main/index.html", context={"articles_list": articles_list})
